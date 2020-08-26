be7a82b8569ba2605825ca9ba87ec6f8b7460fe6
Brandon Rorie <50763955+btror@users.noreply.github.com>
2020-08-25 21:21:05 -0400

    Update HangmanGUI.py
    Added the ability for users to guess a word

--- HangMan/HangmanGUI.py
+++ HangMan/HangmanGUI.py
@@ -29,33 +29,41 @@ top_lines.pack()
 label = Label(master, text="Guess a letter:", font=("Courier", 16))
 label.place(x="165", y="75")
 
-letter_guess = Entry(master, width=3, font=("Courier", 18))
+letter_guess = Entry(master, width=4, font=("Courier", 18))
 letter_guess.place(x="375", y="75")
 
-attempts = 0
+label2 = Label(master, text="Guess the word:", font=("Courier", 16))
+label2.place(x="165", y="120")
+
+word_guess = Entry(master, width=10, font=("Courier", 18))
+word_guess.place(x="375", y="120")
 
 
 def button_click():
     guess = letter_guess.get()
-    print(guess)
-    index = -1
-    locations = []
-    letter_found = False
-    while True:
-        index = game_puzzle.find(guess, index + 1)
-        if index == -1:
-            break
-        locations.append(index)
-        lines[index] = guess + " "
-        letter_found = True
-    x = ' '
-    for k in range(word_length):
-        x += (lines[k])
+    guess_word = word_guess.get()
 
-    top_lines.config(text=x)
+    if len(guess) > 0:
+        index = -1
+        locations = []
+        while True:
+            index = game_puzzle.find(guess, index + 1)
+            if index == -1:
+                break
+            locations.append(index)
+            lines[index] = guess + " "
+        x = ' '
+        for k in range(word_length):
+            x += (lines[k])
+        top_lines.config(text=x)
+    if len(guess_word) > 0:
+        if guess_word == game_puzzle:
+            temp = ''
+            for n in range(word_length):
+                temp += game_puzzle[n] + ' '
+            top_lines.config(text=temp)
 
 
 button_guess = Button(master, text="Submit", font=("Courier", 11), command=button_click)
-button_guess.place(x="440", y="75")
-
+button_guess.place(x="450", y="75")
 mainloop()
