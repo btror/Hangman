d6485dd858f30d81cd276b1239758b6ca71e7f69
Brandon Rorie <50763955+btror@users.noreply.github.com>
2020-09-01 00:13:42 -0400

    Update HangmanGUI.py
    - Added a canvas
    - Added the letter-guessing box
    - Started the hangman animation
    - optimized the entry boxes

--- HangMan/HangmanGUI.py
+++ HangMan/HangmanGUI.py
@@ -17,31 +17,51 @@ top = ''
 for i in range(len(game_puzzle)):
     word_length += 1
     lines.append('_ ')
-    top += '_ '
+    if i == 0:
+        top += ' _ '
+    if i > 0:
+        top += '_ '
 
 master = Tk()
 master.title('Hang-Man')
 master.geometry("700x500")
+master.resizable(False, False)
+
+w = Canvas(master, width=700, height=500, bg='red')
+w.pack()
+
+w.create_rectangle(40, 200, 60, 460, outline="#000000", fill="#000000")
+w.create_rectangle(60, 200, 200, 220, outline="#000000", fill="#000000")
+w.create_rectangle(175, 220, 180, 255, outline="#000000", fill="#000000")
+
+guesses = Label(master, text="Guessed Letters", font=("Courier Bold", 14), bg='red')
+guesses.place(x="460", y="170")
+w.create_rectangle(420, 200, 650, 455, outline="#000000", width="3")
+guess_label = Label(master, text="", font=("Courier Bold", 14), bg='red', wraplength=190, justify=LEFT)
+guess_label.place(x="455", y="205")
+letter_guesses = []
 
 top_lines = Label(master, text=top, font=("Courier", 32))
-top_lines.pack()
+top_lines.place(x="10", y="10")
 
 label = Label(master, text="Guess a letter:", font=("Courier", 16))
-label.place(x="165", y="75")
+label.place(x="10", y="75")
 
-letter_guess = Entry(master, width=4, font=("Courier", 18))
-letter_guess.place(x="375", y="75")
+letter_guess = Entry(master, width=4, font=("Courier", 18), justify='center')
+letter_guess.place(x="220", y="75")
 
 label2 = Label(master, text="Guess the word:", font=("Courier", 16))
-label2.place(x="165", y="120")
+label2.place(x="10", y="120")
 
 word_guess = Entry(master, width=10, font=("Courier", 18))
-word_guess.place(x="375", y="120")
+word_guess.place(x="220", y="120")
 
 
 def button_click():
     guess = letter_guess.get()
     guess_word = word_guess.get()
+    letter_guess.delete(0, 'end')
+    word_guess.delete(0, 'end')
 
     if len(guess) > 0:
         index = -1
@@ -52,6 +72,12 @@ def button_click():
                 break
             locations.append(index)
             lines[index] = guess + " "
+        if guess not in letter_guesses and len(guess) == 1:
+            temp = guess_label['text']
+            temp += '' + guess + '            '
+            guess_label.config(text=temp)
+            letter_guesses.append(guess)
+
         x = ' '
         for k in range(word_length):
             x += (lines[k])
@@ -60,10 +86,13 @@ def button_click():
         if guess_word == game_puzzle:
             temp = ''
             for n in range(word_length):
-                temp += game_puzzle[n] + ' '
+                if n > 0:
+                    temp += game_puzzle[n] + ' '
+                if n == 0:
+                    temp += ' ' + game_puzzle[n] + ' '
             top_lines.config(text=temp)
 
 
 button_guess = Button(master, text="Submit", font=("Courier", 11), command=button_click)
-button_guess.place(x="450", y="75")
+button_guess.place(x="295", y="75")
 mainloop()
